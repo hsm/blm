93b8376032e9f4d9579aa7d594d33a7e26f57020
Henrik Schmidt-Møller <henrik.hsm@gmail.com>
2016-01-19 01:58:20 +0100

    Feature complete

--- R/blm.R
+++ R/blm.R
@@ -1,32 +1,27 @@
 #' @export
 blm <- function(model, prior = NULL, alpha = 1, beta = 1, ...) {
   frame <- model.frame(model, ...)
+  m <- model.matrix(model, frame)
 
   if (is.null(prior)) {
-    d <- dim(frame)[[2]]
+    d <- dim(m)[[2]]
     prior <- list(mean = rep(0, d), covar = alpha * diag(1, nrow = d))
   }
 
-  # to update
-  m <- model.matrix(model, frame)
   covar <- solve(prior$covar + beta * t(m) %*% m)
   mean <- beta * covar %*% t(m) %*% model.response(frame)
   posterior <- list(mean = mean, covar = covar)
 
   structure(list(formula = model,
                  frame = frame,
-                 posterior = posterior),
+                 posterior = posterior,
+                 call = match.call()),
             class = "blm")
 }
 
 distribution <- function(x) UseMethod("distribution")
 distribution.default <- I
 distribution.blm <- function(x) x$posterior
-
-#update <- function(model, prior, ...) {
-  #prior <- distribution(prior)
-  # fitting code here
-#}
 
 namevec <- function(x) {
   result <- as.vector(x)
@@ -38,18 +33,80 @@ coefficients <- function(object, ...) UseMethod("coefficients")
 
 #' @export
 coefficients.blm <- function(object, ...) {
-  namevec(object$posterior$mean)
+  result <- as.vector(object$posterior$mean)
+  names(result) <- rownames(object$posterior$mean)
+  result
 }
 
 #' @export
-predict <- function(object, ...) UseMethod("predict")
-
-#' @export
 predict.blm <- function(object, ...) {
-  responseless.formula <- delete.response(terms(object$formula))
   args <- list(...)
   d <- if (is.null(args$newdata)) object$frame else args$newdata
-  frame <- model.frame(responseless.formula, data = d)
+  predict_on_data(object, d)
+}
+
+predict_on_data <- function(object, data) {
+  responseless.formula <- delete.response(terms(object$formula))
+  frame <- model.frame(responseless.formula, data = data)
   m <- model.matrix(responseless.formula, frame)
   namevec(t(object$posterior$mean) %*% t(m))
+}
+
+#' @export
+residuals.blm <- function(object, ...) {
+  model.response(object$frame) - fitted(object, ...)
+}
+
+#' @export
+fitted.blm <- function(object, ...) {
+  predict_on_data(object, object$frame)
+}
+
+#' @export
+confint.blm <- function(object, level = 0.95, ...) {
+  alpha <- 1 - level
+  mean <- object$posterior$mean
+  sd <- sqrt(diag(object$posterior$covar))
+  critval <- qnorm(1 - alpha / 2, mean, sd)
+  lowerconf <- mean - critval * sd
+  upperconf <- mean + critval * sd
+  result <- cbind(lowerconf, upperconf)
+  colnames(result) <- c(paste(100 * alpha / 2, "%"), paste(100 * (1 - alpha / 2), "%"))
+  result
+}
+
+#' @export
+deviance.blm <- function(object, ...) {
+  sum(residuals(object, ...)^2)
+}
+
+#' @export
+plot.blm <- function(object, ...) {
+  r <- residuals(object, ...)
+  yh <- fitted(object, ...)
+  plot(yh, r, ylim = range(r), main = "Residuals vs Fitted", xlab = paste("Fitted", deparse(object$call), sep = "\n"), ylab = "Residuals")
+  abline(h=0, col=4, lty=2)
+}
+
+#' @export
+print.blm <- function(object, ...) {
+  # Copied and modified from print.lm using getAnywhere('print.lm')
+  cat("\nCall:\n", paste(deparse(object$call), sep = "\n", collapse = "\n"),
+      "\n\n", sep = "")
+  cat("Coefficients:\n")
+  print.default(coefficients(object), print.gap = 2L, quote = FALSE)
+  cat("\n")
+  invisible(object)
+}
+
+#' @export
+summary.blm <- function(object, ...) {
+  print.blm(object, ...)
+  cat("Covariance matrix:\n")
+  print.default(object$posterior$covar, print.gap = 2L, quote = FALSE)
+  cat("\n")
+  cat("Deviance: ", deviance(object, ...), "\n\n")
+  cat("Confidence at 95% level:\n")
+  print.default(confint(object), print.gap = 2L, quote = FALSE)
+  cat("\n")
 }
--- tests/testthat/testblm.R
+++ tests/testthat/testblm.R
@@ -9,7 +9,13 @@ test_that("Prediction matches predicted values from test dataset", {
   expect_equal(prediction, predicted_y, tolerance = 1e-15)
 })
 
-test_that("Prediction works when data is added instead of being bound to formula", {
+test_that("Prediction completes when formula does not have intercept", {
+  simpler <- blm(y ~ x - 1)
+  prediction <- predict(simpler)
+  expect_equal(length(prediction), 100)
+})
+
+test_that("Prediction works when data is added instead of coming from global env", {
   df <- data.frame(a = y, b = x)
   simple <- blm(a ~ b, data = df)
   prediction <- predict(simple)
@@ -24,5 +30,21 @@ test_that("Prediction accepts new data", {
   expect_equal(as.vector(prediction), c(1.516,1.883), tolerance=1e-3)
 })
 
+test_that("Fitted does not accept new data", {
+  simple <- blm(y ~ x)
+  fit <- fitted(simple, newdata = data.frame(x = c(0.59, 0.876)))
+  expect_equal(length(fit), 100)
+})
+
+test_that("Deviance equals the sum of square distances", {
+  simple <- blm(y ~ x)
+  expect_equal(deviance(simple), 8.62412, tolerance=1e-5)
+})
+
+test_that("Confidence intervals are symmetric around the means", {
+  simple <- blm(y ~ x)
+  conf <- confint(simple, level = 0.90)
+  expect_equal(rowMeans(conf), coefficients(simple))
+})
 
 
